cb0912d8eabe18cc1afc60b0de39748f9b7cd73d
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-03-10 14:37:39 -0300

    feat: add operators.js file

---
+++ operators.js
@@ -0,0 +1,22 @@
+// Comparison Operators
+
+// == equal to 
+
+x = 5 
+
+console.log(x == 2) // false
+console.log(2 == 2) // true
+console.log(x == "5") // true
+
+// === equal value and equal type
+
+console.log(x === 5) // true
+console.log(x === "5") // false
+
+// != not equal
+
+console.log(x != 5) // false
+console.log(x != 3) // true
+console.log(x != "5") // false
+
+// !== not equal value or not equal type
