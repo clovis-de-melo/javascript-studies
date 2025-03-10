8046dc27c9ce3568a828cbaa57dd5477b22407a6
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-03-09 23:02:26 -0300

    feat: onclick show & hide

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("test console log");
+
+function show() {
+    document.getElementById('content').style.display = "block";
+}
+
+function hide() {
+    document.getElementById('content').style.display = "none";
+}
