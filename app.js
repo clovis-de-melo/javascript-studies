3eef1867ff5a695181dca3721d089a33e8eb8b16
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-03-09 23:47:45 -0300

    feat: add onclick functions

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-console.log("test console log");
+// console.log("test console log");
 
-function show() {
+// Change Paragraph text´s information
+
+function changeParagraph() {
+    document.getElementById('paragraph').innerHTML = "The content has been changed via JavaScript."
+}
+
+function resetParagraph() {
+    document.getElementById('paragraph').innerHTML = "Click to change the HTML content below"
+}
+
+// Show & Hide content
+
+function showContent() {
     document.getElementById('content').style.display = "block";
 }
 
-function hide() {
+function hideContent() {
     document.getElementById('content').style.display = "none";
+}
+
+// Change Font Size
+
+function changeFontsize() {
+    document.getElementById('content-font-size').style.fontSize = "50px";
+}
+
+function resetFontsize() {
+    document.getElementById('content-font-size').style.fontSize = "16px";
+}
+
+// Change Font Color
+
+function changeFontcolor() {
+    document.getElementById('content-font-color').style.color = "red";
+}
+
+function resetFontcolor() {
+    document.getElementById('content-font-color').style.color = "black";
 }
