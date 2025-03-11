9b5cf6a849823328b9f9c811f66e6d21b3f54ec4
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-03-11 14:33:08 -0300

    feat: add variables.js file

---
+++ variables.js
@@ -0,0 +1,31 @@
+// Variables 
+
+let age = 90 // number type
+let musicName = "Go with the flow" // string type
+let loggedIn = false // boolean type
+
+// Arrays (lista, ordem, arrumar)
+
+let ingredient1 = "milk"
+let ingredient2 = "sugar"
+let ingredient3 = "bread"
+
+let ingredients = ["milk", "sugar", "bread"]
+console.log(ingredients);
+console.log(ingredient1);
+console.log(ingredients[2]); 
+console.log(ingredients[0], ingredients[2]);
+
+// Object
+
+let actor = {
+    name: "Fulano",
+    level: 10,
+    language: "Portuguese",
+    hired: false
+}
+
+console.log(actor.name);
+console.log(actor.level);
+console.log(actor.language);
+console.log(actor.hired);
