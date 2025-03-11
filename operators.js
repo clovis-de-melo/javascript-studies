6caa9ca50e0697fe4d4d645d6f1c0d1aa9e3dde2
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-03-11 09:26:11 -0300

    feat: comparison operators

--- operators.js
+++ operators.js
@@ -1,8 +1,8 @@
 // Comparison Operators
 
-// == equal to 
+x = 5
 
-x = 5 
+// == equal to 
 
 console.log(x == 2) // false
 console.log(2 == 2) // true
@@ -20,3 +20,36 @@ console.log(x != 3) // true
 console.log(x != "5") // false
 
 // !== not equal value or not equal type
+
+console.log(x !== 5) // false
+console.log(x !== "5") // true
+console.log(x !== 8) // true
+
+// > greater than 
+
+console.log(x > 8) // false
+console.log(x < 6) // true
+
+// < less than
+
+console.log(x < 8) // true
+console.log(x < 2) // false
+
+// >= greater than or equal to
+
+console.log(x >= 6) // false
+console.log(x >= 5) // true
+
+// <= less than or equal to 
+
+console.log(x <= 8) // true
+
+
+// How Can it be Used
+//Comparison operators can be used in conditional statements to compare values and take action depending on the result
+
+age = 17
+
+if (age < 18) text = "Too young to buy alcohol";
+console.log(text)
+
