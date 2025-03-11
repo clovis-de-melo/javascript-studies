78428b1be506c85861260914cdcb73890c695421
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-03-11 14:31:58 -0300

    feat: add conditional-statement file

---
+++ conditional-statement.js
@@ -0,0 +1,25 @@
+// Conditional Statement 
+
+// Use if to specify a block of code to be executed, if a specified condition is true
+// Use else to specify a block of code to be executed, if the same condition is false
+// Use else if to specify a new condition to test, if the first condition is false
+// Use switch to specify many alternative blocks of code to be executed
+
+let year = 1987;
+
+if (year >= 1988) {
+    console.log("You are old enough, keep going.")
+} else {
+    console.log("You do not have at least 36 years.")
+}
+
+let person = "John";
+
+
+
+if (person == "John") {
+    console.log("Welcome to the system, John")
+} else {
+    alert("You must have admin access.")
+}
+
