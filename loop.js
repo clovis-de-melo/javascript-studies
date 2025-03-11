1612d1ccb1a2abf426c2c402a8743867af1578f5
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-03-11 14:32:44 -0300

    feat: add loop.js file

---
+++ loop.js
@@ -0,0 +1,15 @@
+// Loop 
+// is a code that can be used several times 
+// there are several types of loops
+
+let list = [10, 20, 30, 40]
+
+for(let item of list) {
+    console.log(item)
+}
+
+let recipe = ["sugar", "water", "salt", "chocolate"]
+
+for(let item of recipe) {
+    console.log(recipe)
+}
