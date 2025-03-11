e997c5177fbcb70f2a856b3219ccbe90156863d4
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-03-11 14:32:27 -0300

    feat: add function.js file

---
+++ function.js
@@ -0,0 +1,27 @@
+// Function 
+
+// input: data that we can send to the function (as long as we want to)
+// process
+// output: result or return 
+
+function addition(a, b) { 
+    let result = a + b;
+    return result 
+}
+
+let x = addition(15, 5);
+let y = addition(2, 8);
+
+console.log(x);
+console.log(y);
+
+// Arrow Function 
+
+let subtraction = (a, b) => {
+    let result = a - b 
+    return result
+} 
+
+let z = subtraction(50, 20);
+console.log(z);
+
