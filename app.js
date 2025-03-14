562de0d2b48014d3fb8d560b6c6cefdf140391e6
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-03-14 18:06:02 -0300

    feat: start currency converter

--- app.js
+++ app.js
@@ -39,3 +39,54 @@ function changeFontcolor() {
 function resetFontcolor() {
     document.getElementById('content-font-color').style.color = "black";
 }
+
+/* alert("test alert") */
+
+// Event Handling 
+// Event Listener
+
+// Using onclick function
+
+function convert() {
+    console.log("convert button is working");
+}
+
+// Using ID
+
+/* let convertButton = document.querySelector("#convert-Button")
+
+document.addEventListener("click", () => {
+    convert()
+}) */
+
+// Get input information 
+
+/* let usdInput = document.querySelector("#usd")
+let brlInput = document.querySelector("#brl")
+
+usdInput.addEventListener("keyup", () => {
+    console.log("type USD field is working")
+    console.log(usdInput.value)
+
+})
+
+brlInput.addEventListener("keyup", () => {
+    console.log("type BRL field is working")
+    console.log(brlInput.value)
+}) */
+
+// Currency Converter 
+
+let dolar = 5.84
+
+let usdInput = document.querySelector("#usd")
+let brlInput = document.querySelector("#brl")
+
+usdInput.addEventListener("keyup", () => {
+
+})
+
+brlInput.addEventListener("keyup"), () => {
+
+}
+
