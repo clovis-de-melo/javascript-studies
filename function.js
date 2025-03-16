0fdddd38e6063873a3b027a780b3129e23d20d3c
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-03-16 18:44:25 -0300

    feat: Roger Melo tutorial

--- function.js
+++ function.js
@@ -25,3 +25,47 @@ let subtraction = (a, b) => {
 let z = subtraction(50, 20);
 console.log(z);
 
+// Roger Melo | JavaScript (YouTube Channel)
+
+// () 
+
+// {} function´s block: this where we put the function´s content  
+
+// value that represents the lack of value
+
+function nome () {
+
+}
+
+// Evoke, call or execute a function 
+
+nome()
+
+console.log(nome()) // undefined   
+console.log(nome) // [Function: nome]
+
+//
+
+let numero = 1 
+
+function increment () {
+    numero++ 
+}
+
+increment()
+
+console.log(numero) // 2
+
+increment()
+increment()
+increment()
+
+console.log(numero) // 5 
+
+// functions create scope 
+// scope is the visibility of a value 
+
+function x () {
+    const myConst = 'valor'
+}
+
