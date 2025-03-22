a9ce72fd77bfe1ff4c7878a24244764f194c0485
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-03-21 23:41:42 -0300

    feat: DOM definition

--- dom.js
+++ dom.js
@@ -1,2 +1,5 @@
 // DOM (Document Object Model)
 
+// JavaScript DOMis the programming interface that allows you to manipulate elements of a web page using the JavaScript programming language. 
+
+// 
