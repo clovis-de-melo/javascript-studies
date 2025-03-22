fcb67fa8c3df858f02e8528d99ab4f5880e21967
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-03-21 23:47:23 -0300

    feat: DOM def comment

--- dom.js
+++ dom.js
@@ -2,4 +2,6 @@
 
 // JavaScript DOMis the programming interface that allows you to manipulate elements of a web page using the JavaScript programming language. 
 
-// 
+// The DOM is a structured representation of an HTML, XML, or SVG document. 
+
+// The DOM is a programming interface that allows scripts to interact with the structure, content, and style of web documents. 
