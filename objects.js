64cf5b3e5c1d66b6bc26711cc84e28c755561782
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-04-07 22:38:55 -0300

    feat: new js object

--- objects.js
+++ objects.js
@@ -2,6 +2,8 @@
 
 // A JavaScript object is a collection of key-value pairs, where keys are strings (or symbols) and values can be of any data type, including other objects. Objects are fundamental building blocks in JavaScript, used to represent data structures and encapsulate related data and functionality
 
+// Creating objects
+
 const person = {
     name: "John Doe",
     age: 30,
@@ -9,3 +11,12 @@ const person = {
 };
 
 console.log(person["age"]);
+console.log(person.name);
+
+// New object 
+
+const secondPerson = new Object();
+    person.name = "John Doe";
+    person.age = 30;
+    person.city = "New York";
+
