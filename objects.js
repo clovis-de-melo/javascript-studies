810500e461111c12f41a2c34100401fb94efdc7b
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-04-07 22:34:56 -0300

    feat: add js object file & code example

---
+++ objects.js
@@ -0,0 +1,11 @@
+// JS Objects 
+
+// A JavaScript object is a collection of key-value pairs, where keys are strings (or symbols) and values can be of any data type, including other objects. Objects are fundamental building blocks in JavaScript, used to represent data structures and encapsulate related data and functionality
+
+const person = {
+    name: "John Doe",
+    age: 30,
+    city: "New York"
+};
+
+console.log(person["age"]);
