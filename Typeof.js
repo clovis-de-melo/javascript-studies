de46dbbeae1b0373569c1ec47ba718a827af75a2
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-04-12 16:57:05 -0300

    feat: add Typeof examples

--- Typeof.js
+++ Typeof.js
@@ -2,5 +2,26 @@
 
 // The typeof operator returns the data type of a JavaScript variable.
 
+// In JavaScript, a primitive value is a single value with no properties or methods.
 
+// JavaScript has 7 primitive data types:
 
+// string
+// number
+// boolean
+// bigint
+// symbol
+// null
+// undefined
+// The typeof operator returns the type of a variable or an expression.
+
+typeof "John"         // Returns string
+typeof ("John"+"Doe") // Returns string
+typeof 3.14           // Returns number
+typeof 33             // Returns number
+typeof (33 + 66)      // Returns number
+typeof true           // Returns boolean
+typeof false          // Returns boolean
+typeof 1234n          // Returns bigint
+typeof Symbol()       // Returns symbol
+typeof x              // Returns undefined
