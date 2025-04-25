639bc19d9083e0399f916e5445f31b71f39f581c
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-04-25 17:41:43 -0300

    feat: querySelector variables

--- DOM/dom.js
+++ DOM/dom.js
@@ -6,3 +6,17 @@
 
 // The DOM is a programming interface that allows scripts to interact with the structure, content, and style of web documents. 
 
+function buttonJS(){
+    console.log("JavaScript button is working");
+}
+
+// querySelector 
+
+let header = document.querySelector('#dashboardHeader');
+console.log(header.textContent);
+
+let firstParagraph = document.querySelector('p');
+console.log(firstParagraph.textContent);
+
+let headingThree = document.querySelector('h3');
+console.log(headingThree.textContent);
