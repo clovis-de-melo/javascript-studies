fdae951eafe8bdd2fc87ffe4189f613379fa7785
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-05-05 12:23:31 -0300

    feat: quote inside quotes string outcome

--- data-types/string.js
+++ data-types/string.js
@@ -8,14 +8,38 @@
 
 // There is no difference between the two.
 
-"this is a string"
-
-"12"
-
-12
-
 console.log(12);
 
 let text = "John Doe";
 console.log(text);
 
+
+// Quotes inside quotes 
+
+// You can use quotes inside a string, as long as they don't match the quotes surrounding the string
+
+console.log("This is Joe's favorite string");
+// console.log("This is Joe's "favorite" string"); // This will not work 
+console.log("This is Joe's \"favorite\" string");
+// console.log("This is Joe's \"favorite"\ string"); // This will not work 
+console.log("This is Joe's 'favorite' string");
+
+
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"'; 
+
+/* document.getElementById("string-demo").innerHTML = answer1;
+
+console.log(answer1);
+console.log(answer2);
+console.log(answer3); */
+
+
+// String Properties 
+
+var myString = "This is my string";
+
+console.log(myString.length);
+console.log(myString.charCodeAt);
+console.log(myString.valueOf);
