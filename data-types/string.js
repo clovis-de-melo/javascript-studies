20d294b65cb1c79d89b0e39160ffe20a212861ba
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-05-05 11:39:48 -0300

    feat: add string data type folder and file

---
+++ data-types/string.js
@@ -0,0 +1,21 @@
+// JS String
+
+// Strings are for storing text. Strings are written with quotes
+
+// A JavaScript string is zero or more characters written inside quotes.
+
+// Strings created with single or double quotes work the same.
+
+// There is no difference between the two.
+
+"this is a string"
+
+"12"
+
+12
+
+console.log(12);
+
+let text = "John Doe";
+console.log(text);
+
