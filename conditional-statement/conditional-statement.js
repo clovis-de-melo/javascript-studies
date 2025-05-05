5526cc7fc07b33c96cec0780a20b4c6db58c4f9a
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-05-05 18:03:41 -0300

    feat: add window prompt var

--- conditional-statement/conditional-statement.js
+++ conditional-statement/conditional-statement.js
@@ -23,3 +23,36 @@ if (person == "John") {
     alert("You must have admin access.")
 }
 
+var answer = window.prompt("Type Yes, No or Maybe. Then click Ok.");
+if (answer === "Yes") {
+    console.log("You said yes");
+} else if (answer === "Maybe") {
+    console.log("You said maybe")
+} else if (answer === "No") {
+    console.log("You said no");
+} else {
+    console.log("Could not get your answer");
+}
+
+
+// Conditionals If
+
+var answer = window.prompt("Second prompt: Type YES, NO, or MAYBE. Then click OK.");
+if (answer === "YES" || answer === "NO"){
+    console.log("Yes || No is working")
+}
+if (answer === "YES") {
+    console.log("You said YES!");
+}
+else {
+    console.log("")
+}
+
+// Conditionals: Switch 
+
+var answer = window.prompt("Third prompt: Type YES, NO, or MAYBE. Then click OK.");
+switch (answer) {
+    case "YES":
+        console.log("Switch says: The answer is YES");
+        break;
+}
