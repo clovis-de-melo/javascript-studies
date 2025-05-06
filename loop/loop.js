3097a3ad741821fc997a3488c841db6c71bbf1cd
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-05-06 18:08:43 -0300

    feat: lopping over an arrey

--- loop/loop.js
+++ loop/loop.js
@@ -14,3 +14,30 @@ for(let item of recipe) {
     console.log(recipe)
 }
 
+//
+
+for (let i = 0; i < 10; i += 1) {
+    console.log(i)
+}
+
+// Very common use case: looping over an arrey 
+
+var pageNames = [
+    "Home",
+    "About us",
+    "Contact us",
+    "JavaScript Playground",
+    "News",
+    "JavaScript Loop",
+    "Blog"
+];
+
+for (i = 0; i < pageNames.length; i += 1) {
+    if (document.title === pageNames[i]) {
+        console.log("We are here: " + pageNames[i]);
+    break;    
+    } else {
+        console.log("We are not here: " + pageNames[i]);
+    }
+}    
+    
