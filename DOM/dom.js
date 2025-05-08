ec8dfdc05da9930df2a3ab2401f06f68d5a014de
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-05-08 11:58:10 -0300

    feat: challenge solution

--- DOM/dom.js
+++ DOM/dom.js
@@ -50,3 +50,31 @@ elements.forEach(element => {
         console.log('Found active element:', element);
     }
 })
+
+// Challenge 1: Select an element by ID and print it to the console
+// TODO: Select the element with id 'actionButton' and print it
+
+let actionButton = document.getElementById('actionButton');
+console.log(actionButton);
+
+// Challenge 2: Select all elements with a certain class and print all of them to the console
+// TODO: Select all elements with class 'info' and print each one
+
+let infoElements = document.getElementsByClassName('info');
+for (let i = 0; i < infoElements.length; i++) {
+    console.log(infoElements[i]);
+}
+
+// Challenge 3: Select elements by tag and print all of them to the console
+// TODO: Select all <p> elements and print each one
+
+let paragraphs = document.getElementsByTagName('p');
+for (let i = 0; i < paragraphs.length; i++) {
+    console.log(paragraphs[i]);
+}
+
+// Challenge 4: Select elements with a combination of tag name and class name, and print them
+// TODO: Select all elements that are <span> with class 'highlight' and print each one
+
+let highlightedSpans = document.querySelectorAll('span.highlight');
+highlightedSpans.forEach(span => console.log(span));
