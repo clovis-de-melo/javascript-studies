b58d5e946969cc342eeb602abdf901eb106bd44a
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-05-16 19:53:52 -0300

    feat: displayDate function addEventListener

--- DOM/dom.js
+++ DOM/dom.js
@@ -186,5 +186,10 @@ contentArea.appendChild(newParagraph);
 
 // Event listener & Event handler 
 
+// <button id="myBtn">Try it</button>
 
+document.getElementById("myBtn").addEventListener("click", displayDate);
 
+function displayDate() {
+  document.getElementById("demo").innerHTML = Date();
+}
